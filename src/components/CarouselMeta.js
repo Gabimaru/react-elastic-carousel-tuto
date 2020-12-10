15162ff3938a561c1f4c5cf591a24f99fbc1a4b1
Gabimaru <aashrader@yahoo.fr>
2020-12-10 09:52:35 +0100

    img folder

--- src/components/CarouselMeta.js
+++ src/components/CarouselMeta.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import Carousel from "react-elastic-carousel"
 
-import Metamask1 from "./img/MetaMask11.png"
-import Metamask2 from "./img/MetaMask22.png"
-import Metamask3 from "./img/MetaMask44.png"
-import Metamask4 from "./img/MetaMask55.png"
+// Import des slides qui composent le carousel depuis le dossier local "img"
+import Metamask1 from "./../img/MetaMask11.png"
+import Metamask2 from "./../img/MetaMask22.png"
+import Metamask3 from "./../img/MetaMask44.png"
+import Metamask4 from "./../img/MetaMask55.png"
 
-import browser1 from "./img/browserFirefox.png"
-import browser2 from "./img/browserchrome.png"
-import browser3 from "./img/browserBrave.png"
-import browser4 from "./img/browserEdge.png"
+// Import des icônes qui composent la 3ème slide du carousel depuis le dossier local "img"
+import browser1 from "./../img/browserFirefox.png"
+import browser2 from "./../img/browserchrome.png"
+import browser3 from "./../img/browserBrave.png"
+import browser4 from "./../img/browserEdge.png"
 
-
-function CarouselMeta() {
+// Seul et unique fonction fléchée du compoent 'CarouselMeta'
+const CarouselMeta = () => {
   return (
     <div className="container" >
       <Carousel >
 
+{/*Paragraphe de code de la 1ère slide*/}
           <div style={{position:"relative"}}>
               <img className="card-title1" src={Metamask1} alt="Prérequis à l'utilisation de la blockchain" />
               <div style={{position:"absolute", top:"50px", left:"550px", width:"600px", height:"400px", fontSize:"200%"}}>
@@ -27,24 +30,27 @@ function CarouselMeta() {
               </div> 
           </div>
 
+{/*Paragraphe de code de la 2ème slide*/}
           <div style={{position:"relative"}}>
               <img className="card-title2" src={Metamask2} alt="Le wallet MetaMask"/>
               <div style={{position:"absolute", top:"150px", left:"650px", width:"500px", height:"400px", fontSize:"200%"}}>
-                <p><b>Pour nos premiers pas dans la blockchain, nous utiliserons le wallet sécurisé et respectueux des données privées : MetaMask.</b></p>                
+                <p><b>Pour nos premiers pas dans la blockchain, nous utiliserons le wallet : MetaMask.</b></p>                
                 <p><b>Nous reviendrons en détail sur son principe de fonctionnement plus tard. Installons le sans plus attendre.</b></p>
               </div>
           </div>
 
+{/*Paragraphe de code de la 3ème slide*/}
           <div style={{position:"relative"}}>
               <img className="card-title4" src={Metamask3} alt="Se procurer des ETH de test"/>
               <div style={{position:"absolute", top:"30px", left:"220px", width:"525px", height:"400px", fontSize:"300%"}}>
                 <p className="pb-5"><b>METAMASK</b></p>
               </div>
-              <div style={{position:"absolute", top:"100px", left:"675px", width:"525px", height:"400px", fontSize:"200%"}}>
-                <p className="pb-5"><b>Ce qui est génial avec MetaMask c'est qu'il s'intègre dans votre navigateur web très facilement.</b></p>
+              <div style={{position:"absolute", top:"150px", left:"675px", width:"525px", height:"400px", fontSize:"200%"}}>
+                <p><b>MetaMask s'intègre dans votre navigateur web très facilement.</b></p>
               </div>
               <div style={{position:"absolute", top:"300px", left:"675px", width:"525px", height:"400px", fontSize:"200%"}}>
                 <p className="pb-4"><b>Il nous suffit de l'ajouter en cliquant sur l'une des icônes suivantes en fonction du navigateur utilisé.</b></p>
+{/*Paragraphe des icônes cliquables de la 3ème slide*/}                
                 <div className="container py-4">
                   <a href="https://addons.mozilla.org/fr/firefox/addon/ether-metamask/" target="_blank" rel="noreferrer">
                     <img src={browser1} alt="icône de firefox" className="mx-2" />
@@ -62,6 +68,7 @@ function CarouselMeta() {
             </div>
           </div>
 
+{/*Paragraphe de code de la 4ème slide*/}
           <div style={{position:"relative"}}>
               <img className="card-title4" src={Metamask4} alt="Se procurer des ETH de test"/>
               <div style={{position:"absolute", top:"190px", left:"650px", width:"500px", height:"400px", fontSize:"200%"}}>
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Main = () => {
   return (
-    <div className="test">
+    <main>
 
-      </div>
+    </main>
   );
 };
 
